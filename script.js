5fbd4677ff7972b9bb281f6edfec4142517e5c1f
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-07-24 22:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function isSameType(value1, value2) {
 
     if (Number.isNaN(value1) && Number.isNaN(value2)) {
         return true;
-    }
+    }else{
+		return false;
+	}
     const result = typeof value1 === typeof value2;
     
     return result;
